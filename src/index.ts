c7762634bb316516d7784a48ace3b622dacac586
Wyyyz <nmmlwc@gmail.com>
2023-12-11 10:08:06 +0800

    没啥更改的

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Fs from "fs";
 import {baseDir} from "./client";
-
+export * from './common'
 export {Client, createClient, createClientByCache} from './client';
 export * from './entity/entities';
 export {Qrcode} from './utils';
