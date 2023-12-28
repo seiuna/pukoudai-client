3f6d78f8c7799c4b269a870eededc522702bec13
Wyyyz <nmmlwc@gmail.com>
2023-12-28 22:16:34 +0800

    额？

--- src/o/sign.ts
+++ src/o/sign.ts
@@ -1,7 +1,5 @@
 import {MD5} from 'crypto-js';
-// 20871749 4462300 1698216891 s25ycjfxcehwzs60yookgq8fx1es05af
-//    uid     eventid      当前时间戳              keys(固定?)
-//组合后MD5 全小写为 form表单 sign字段
+
 const keys = "s25ycjfxcehwzs60yookgq8fx1es05af"
 
 //签名算法 逆天混淆
